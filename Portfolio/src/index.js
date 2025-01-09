2453783d1a632d9d84441637c4ebc83b2977426f
Kai <kharimobley@gmail.com>
2025-01-08 20:38:19 -0500

    First experimental animations

--- Portfolio/src/index.js
+++ Portfolio/src/index.js
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js"
+import getStarfield from "../assets/helpers/getStarfield.js"
+import { getFresnelMat } from "../assets/helpers/getFresnelMat.js"
 
+/* DUNE SPHERE
 // Get dimensions of browser window
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -32,9 +35,9 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 // Specify geometry, color and material - which goes into mesh
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
+const geo = new THREE.SphereGeometry(1, 32, 16);
+const mat = new THREE.MeshDistanceMaterial({
+  color: 0xfffff,
   flatShading: true
 });
 
@@ -62,4 +65,65 @@ function animate(t = 0) {
   renderer.render(scene, camera);
   controls.update();
 }
+animate();
+*/
+
+// Earth 
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
+
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+const loader =new THREE.TextureLoader();  
+const geometry = new THREE.IcosahedronGeometry(1, 12);
+const material = new THREE.MeshStandardMaterial({
+  map: loader.load("../assets/textures/00_earthmap8k.jpg"),
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
+
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("../assets/textures/03_earthlights10k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+
+const cloudsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("../assets/textures/04_earthcloudmap2k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+
+earthGroup.add(lightsMesh);
+earthGroup.add(glowMesh);
+//earthGroup.add(cloudsMesh);
+
+const stars = getStarfield({ numStars: 3500 }); 
+scene.add(stars);
+
+const sunLight = new THREE.DirectionalLight(0xfffff);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
+function animate() {
+  requestAnimationFrame(animate);
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.002;
+  glowMesh.rotation.y += 0.002;
+  renderer.render(scene, camera);
+}
+
 animate();
